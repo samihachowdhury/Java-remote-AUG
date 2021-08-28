28c0c4817a7cd8c3f51fc66468f8ff826620c3fd
Samiha Chowdhury <chowdhuryhsamiha@gmail.com>
2021-08-28 01:25:36 -0400

    Update scriptjs.js

--- 8-26-21-theDOM/scriptjs.js
+++ 8-26-21-theDOM/scriptjs.js
@@ -1,39 +1,40 @@
-// let el = document.getElementById("username");
-// let inputs = document.getElementsByTagName("input");
-// let inError = document.getElementsByClassName("error");
 
-// console.log(inputs.length);
-// console.log(inError.length);
+// let el = document.getElementById('username');
+// let inputs = document.getElementsByTagName('input')
+// let inError = document.getElementsByClassName('error')
+
+
+// console.log(inputs.length)
+// console.log(inError.length)
 
 
 // let firstButton = document.querySelector('button')
 // let inError = document.querySelectorAll('input.error')
 
+
 // console.log(firstButton)
 // console.log(inError)
 
 
-
-
-
-// let links = document.querySelectorAll('a');
+// let links = document.querySelectorAll('a')
 // let arrayOfLinks = Array.from(links)
 
-// let linkCount = links.length
+// let linkCount = links.length;
 
-// let firstLink = links[0]
+// let fitstLink = links[0]
+
+
 
 // links.forEach(function(link){
 
 // })
 
-// to get the stuff that's attached to it (traverse)
 
-// let listItems = document.querySelector('ul').children 
+// let listItems = document.querySelector('ul').children
+
 // console.log(listItems.length)
 
 // let selectedItem = document.querySelector('li.selected')
-// console.log(selectedItem)
 
 // let first = selectedItem.previousElementSibling;
 // let last = selectedItem.nextElementSibling;
@@ -45,60 +46,60 @@
 // console.log(section)
 
 
-// let div = document.querySelector('div');
-
-// let a = document.querySelector('a');
+// let div = document.querySelector('div')
+// let a = document.querySelector('a')
 
 // console.log(a.innerHTML)
 // console.log(div.innerHTML)
 
 // a.innerHTML = "Updated link text"
 
+
 // let a = document.querySelector('a')
+
 // console.log(a.href)
+
 
 // a.name = 'new link name'
 
 // a.target = "_blank"
 
-// //Remove the first list item
+// document.querySelector('.first').remove()
 
-// document.querySelector('.first').remove();
+////////// Remember this  tag for the home work.........
 
-
-
-//////////// Remember this tag for the homework 
-
-// // create the new html tag 
+// create the new html tag
 // let newLi = document.createElement('li')
-// // add the text for the new html tag 
+// // add the text for the new html tag
 // newLi.innerHTML = "item 2"
-// // select the area to attach the tag
+
+// // select the area to attaach the tag
 // let list = document.querySelector('ul')
-// // add the tag to the selected parent tag 
-// list.appendChild(newLi) 
+// // add the tag to the selected child tag to the parent tag
+// list.appendChild(newLi)
 
 
-// let el = document.getElementById('myEl')
+let el = document.getElementById('myEl')
 
 
-// el.addEventListener('click',function(event){
-//     console.log('clicked')
-// })
+el.addEventListener('click', function(event){
+    console.log('clicked')
+})
 
-// el.addEventListener('mouseover', function(event){
-//     el.innerHTML = 'hovered over button'
 
-// })
+el.addEventListener('mouseover', function(event){
+    el.innerHTML = 'hovered over button'
+})
 
 
 
 document.querySelector('.outer').addEventListener('click', function(event){
-        // e represents the event (element that has been clicked)
-	console.log('target', event.target)
-        console.log(event.target, event.currentTarget);
-	//inner, outer
+console.log('target',event.target)
+console.log('currentTarget',event.currentTarget)
+
 })
+
+
 
 
 
