f78faa08ff05dda7ed23a4f33e3a13a87aaa6371
Samiha Chowdhury <chowdhuryhsamiha@gmail.com>
2021-09-06 00:07:55 -0400

    Create Main.java

---
+++ Homeworks/Week4/Java/Java101Hw/src/com/company/Main.java
@@ -0,0 +1,51 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+          String name = "Samiha";
+          System.out.println(name.toLowerCase());
+
+//        String num = "2334736";
+//        System.out.println(num);
+
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("Hi, Samiha! What is your major?");
+//        String Major = input.nextLine();
+//
+//        System.out.println("When is your graduation date?");
+//        String GraduationDate = input.nextLine();
+//
+//        System.out.println("Which state do you live in?");
+//        String State = input.nextLine();
+//
+//        int score;
+//
+//        score = 99;
+//        System.out.println(score);
+//        double price;
+//        price = 2.99;
+//        System.out.println(price);
+//
+//        boolean lose ;
+//        lose = false;
+//
+//        System.out.println(lose);
+
+//        int pepsi, fanta, sprite;
+//
+//        int pepsi;
+//        int fanta;
+//        int sprite;
+
+//        int pepsi=0, fanta=0, sprite =0;
+
+//        int pepsi = 0;
+//        int fanta = 0;
+//        int sprite = 0;
+
+    }
+}
